6cf1168246ac05bf60f3ca09d647a1950016f853
Sávio Lucena <saviogl@gmail.com>
2016-06-29 10:44:31 -0300

    Add MainController HomePage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :links
 
   get ':tiny' => 'links#redirect'
-  root 'links#index'
+  root 'main#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

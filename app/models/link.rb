2327fd7654f98a1d0c66e941fa1eb9ad8492fcb0
Sávio Lucena <saviogl@gmail.com>
2016-07-04 14:08:42 -0300

    Change URL regex

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,5 @@
 class Link < ActiveRecord::Base
-  validates :original, presence: true, format: { with: URI.regexp, message: "Invalid URI" }
+  validates :original, presence: true, format: { with: /\A^((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$\z/i, message: "Invalid URL" }
 
   after_create :tinify
 

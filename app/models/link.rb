402190838e5720d9971d33f63cc3b38eb5314498
Sávio Lucena <saviogl@gmail.com>
2016-07-04 14:20:15 -0300

    Add URL protocol

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,7 @@ class Link < ActiveRecord::Base
   validates :original, presence: true, format: { with: /\A^((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$\z/i, message: "Invalid URL" }
 
   after_create :tinify
+  before_create :smart_add_url_protocol
 
   def getTinyUrl
     "#{Rails.application.config.appHost}/#{self.tinyfied}"
@@ -16,5 +17,11 @@ class Link < ActiveRecord::Base
     def tinify
       self.tinyfied = HashidsI.encode(self.id)
       self.save
+    end
+
+    def smart_add_url_protocol
+      unless self.original[/\Ahttp:\/\//] || self.original[/\Ahttps:\/\//]
+        self.original = "http://#{self.original}"
+      end
     end
 end
